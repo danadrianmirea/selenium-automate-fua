9cc7febf4189b304d8d77c28e21f56334ef9678d
Dan Adrian Mirea <damirea84@gmail.com>
2024-12-08 11:10:37 +0200

    remove init.bat

--- init.bat
+++
@@ -1,7 +0,0 @@
-echo "# selenium-automate-fua" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:danadrianmirea/selenium-automate-fua.git
-git push -u origin main
